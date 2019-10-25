bea9b52295838ca63c3fce0f107edf426727a809
Seref <Seref.G98@gmail.com>
2019-10-25 06:05:20 +0200

    Simple 2D RPG System

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+
+	public bool lockCursor;
+	public Transform target;
+
+	public float smoothSpeed = 0.125f;
+
+	public Vector2 cameraBounds = new Vector2(2, 2);
+
+	private Bounds currentBoundingBox;
+	private Vector3 desiredPosition;
+
+
+	void Start()
+	{
+		currentBoundingBox = new Bounds(new Vector2(transform.position.x, transform.position.y), cameraBounds);
+		desiredPosition = target.position;
+
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (!currentBoundingBox.Contains(new Vector2(target.position.x, target.position.y))) { 
+			desiredPosition = target.position;
+			currentBoundingBox = new Bounds(new Vector2(target.position.x, target.position.y), cameraBounds);
+		}
+
+		Vector3 smoothPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed);
+		transform.position = new Vector3(smoothPosition.x, smoothPosition.y, transform.position.z);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+
+	public float walkSpeed = 2;
+	public float runSpeed = 6;		
+	
+	void LateUpdate()
+	{
+		// input
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);		
+		bool running = Input.GetKey(KeyCode.LeftShift);
+
+		Move(input, running);		
+	}
+
+	void Move(Vector3 input, bool running)
+	{								
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * input.magnitude;
+		Vector2 direction = input.normalized;
+
+		transform.position = new Vector3(transform.position.x + direction.x * targetSpeed, transform.position.y + direction.y * targetSpeed, transform.position.z);
+				
+	}	
+	
+}
