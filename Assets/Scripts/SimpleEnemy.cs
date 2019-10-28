c736de634cda670120708404d951c2d7de41c2d1
Seref <Seref.G98@gmail.com>
2019-10-28 16:39:21 +0100

    +

---
+++ Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleEnemy : MonoBehaviour
+{
+    NavMeshAgent navagent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navagent = GetComponent<NavMeshAgent>();
+        navagent.destination = new Vector3(0, 0,0 );
+    }
+
+}
---
+++ Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayer : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    
+
+    void LateUpdate()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
+        transform.LookAt(new Vector3(mousePos.x, 0, mousePos.y));
+    }
+}
