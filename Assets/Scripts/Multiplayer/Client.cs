810929e2b81309a2a5e7c03b025ebdec22866dc2
Seref <Seref.G98@gmail.com>
2019-10-30 13:43:03 +0100

    partial Servercode

--- Assets/Scripts/Multiplayer/Client.cs
+++ Assets/Scripts/Multiplayer/Client.cs
@@ -7,110 +7,114 @@ using UnityEngine;
 public class Client : MonoBehaviour
 {
 
-	// define public game object used to visualize other players
-	public GameObject enemyObject;
-	public GameObject playerObject;
-	public GameObject bulletObject;
-	public float smoothUpdates = 10f;
-	private Vector3 prevPosition;
-	private Quaternion prevRotation;
+    // define public game object used to visualize other players
+    public GameObject enemyObject;
+    public GameObject playerObject;
+    public GameObject bulletObject;
+    public float smoothUpdates = 10f;
+    private Vector3 prevPosition;
+    private Quaternion prevRotation;
 
-	public GameObject bullets;
-	public GameObject player;
-	public GameObject enemies;
+    public GameObject bullets;
+    public GameObject player;
+    public GameObject enemies;
 
-	public EnemyList en;
-	private Dictionary<int, GameObject> objectlist;
-	IEnumerator Start()
-	{		
-		//TextMeshProUGUI t = GameObject.Find("Text_Address").GetComponent<TextMeshProUGUI>();
-		// connect to server
-		WebSocket w = new WebSocket(new Uri("ws://localhost;"));//+t.text));
-		yield return StartCoroutine(w.Connect());
-		Debug.Log("CONNECTED TO WEBSOCKETS");
+    public readonly Dictionary<int, GameObject> gameObjectList = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> objectlist;
+    IEnumerator Start()
+    {
+        //TextMeshProUGUI t = GameObject.Find("Text_Address").GetComponent<TextMeshProUGUI>();
+        // connect to server
+        WebSocket w = new WebSocket(new Uri("ws://localhost;"));//+t.text));
+        yield return StartCoroutine(w.Connect());
+        Debug.Log("CONNECTED TO WEBSOCKETS");
 
-		// generate random ID to have idea for each client (feels unsecure)
-		System.Guid myGUID = System.Guid.NewGuid();
-		int playerID = myGUID.GetHashCode();
+        // generate random ID to have idea for each client (feels unsecure)
+        System.Guid myGUID = System.Guid.NewGuid();
+        int playerID = myGUID.GetHashCode();
 
-		objectlist = new Dictionary<int, GameObject>();
-		
-		// wait for messages
-		while (true)
-		{
-			// read message
-			string message = w.RecvString();
-			// check if message is not empty
-			if (message != null)
-			{				
+        objectlist = new Dictionary<int, GameObject>();
 
-				ClientData data = JsonUtility.FromJson<ClientData>(message);				
+        // wait for messages
+        while (true) {
+            // read message
+            string message = w.RecvString();
+            // check if message is not empty
+            if (message != null) {
 
-				switch (data.type)
-				{
-					case (int)DataType.Enemy:
-						//TODO: implement Other Player Bullets
+                DataGroup data = JsonUtility.FromJson<DataGroup>(message);
+                foreach (DataWrapper package in data.dataList) {
 
-						ClientData enemyData = data;
-						if (!objectlist.ContainsKey(enemyData.id))
-						{
-							GameObject enemy = Instantiate(enemyObject, enemyData.position, enemyData.rotation);
-							enemies.transform.parent = enemy.transform.parent;
-							objectlist.Add(enemyData.id, enemy);
-						}
-						else {
-							GameObject enemy = objectlist[enemyData.id];
-							enemy.transform.position = enemyData.position;
-							enemy.transform.rotation = enemyData.rotation;
-						}
-						break;
-					case (int)DataType.Bullet:
-						//TODO: implement Other Player Bullets
-						ClientData bulletData = data;
-						if (!objectlist.ContainsKey(bulletData.id))
-						{
-							GameObject bullet = Instantiate(bulletObject, bulletData.position, bulletData.rotation);
-							bullets.transform.parent = bullet.transform.parent;
-							objectlist.Add(bulletData.id, bullet);
-						}
-						else
-						{
-							GameObject bullet = objectlist[bulletData.id];
-							bullet.transform.position = bulletData.position;
-							bullet.transform.rotation = bulletData.rotation;
-						}
-						break;
-					case (int)DataType.Player:
-						ClientData playerData = data;
-						if (!objectlist.ContainsKey(playerData.id))
-						{
-							GameObject playertemp = Instantiate(playerObject, playerData.position, playerData.rotation);
-							player.transform.parent = playertemp.transform.parent;
-							objectlist.Add(playerData.id, playertemp);
-						}
-						else
-						{
-							GameObject playertemp = objectlist[playerData.id];
-							playertemp.transform.position = playerData.position;
-							playertemp.transform.rotation = playerData.rotation;
-						}
-						break;
-					default:
-						break;
-				}				
-			}
+                    GameObject gameObject = null;
+                    if (gameObjectList.ContainsKey(package.objectID)) {
+                        gameObject = gameObjectList[package.objectID];
+                    }
+                    if (gameObject == null) {
+                        if (package.type.Equals(DataType.DataPosition)) {
+                            DataPosition dataPosition = JsonUtility.FromJson<DataPosition>(package.data);
+                            switch (dataPosition.prefabType) {
+                                case DataPrefabType.ENEMY:
+                                    gameObject = Instantiate(enemyObject, dataPosition.position, dataPosition.rotation);
+                                    enemies.transform.parent = gameObject.transform.parent;
+                                    break;
+                                case DataPrefabType.PLAYER:                              
+                                    bullets.transform.parent = gameObject.transform.parent;
+                                    player = Instantiate(playerObject, dataPosition.position, dataPosition.rotation);
+                                    break;
+                                case DataPrefabType.BULLETS:
+                                    gameObject = Instantiate(bulletObject, dataPosition.position, dataPosition.rotation);
+                                    bullets.transform.parent = gameObject.transform.parent;
+                                    break;
+                            }
 
-			// if connection error, break the loop
-			if (w.error != null)
-			{
-				Debug.LogError("Error: " + w.error);
-				break;
-			}			
+                            gameObjectList.Add(package.objectID, gameObject);
 
-			yield return 0;
-		}
+                            continue;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
 
-		// if error, close connection
-		w.Close();
-	}
+                    switch (package.type) {
+                        case DataType.DataPosition:
+                            DataPosition dataPosition = JsonUtility.FromJson<DataPosition>(package.data);
+                            gameObject.transform.position = dataPosition.position;
+                            gameObject.transform.rotation = dataPosition.rotation;
+
+                            break;
+                        case DataType.DataStatus:
+                            DataStatus dataStatus = JsonUtility.FromJson<DataStatus>(package.data);
+                            switch (dataStatus.dataState) {
+                                case DataState.ACTIVE:
+                                    gameObject.SetActive(true);
+                                    break;
+                                case DataState.INACTIVE:
+                                    gameObject.SetActive(false);
+                                    break;
+                                case DataState.REMOVED:
+                                    Destroy(gameObject);
+                                    gameObjectList[package.objectID] = null;
+                                    break;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+
+            // if connection error, break the loop
+            if (w.error != null) {
+                Debug.LogError("Error: " + w.error);
+                break;
+            }
+
+            yield return 0;
+        }
+
+        // if error, close connection
+        w.Close();
+    }
 }
--- Assets/Scripts/Multiplayer/Host.cs
+++ Assets/Scripts/Multiplayer/Host.cs
@@ -3,183 +3,196 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum DataType { Enemy, Player, Bullet };
 
-// define classed needed to deserialize recieved data
+/// <summary> Datatypes that can be used (as of now)
+/// </summary>
+public enum DataType { DataPosition, DataStatus };
+
+/// <summary> DataWrapper structure each information is packed inside one "package"
+///    with an additional space for various payloads
+/// </summary>
+/// <param name="objectID">unique ID of every Gameobject that needs to be updated on the server</param>
+/// <param name="host">to define the direction Host -> Client or Client -> Host </param>
+/// <param name="type">define a Type to allow for multiple different JSON structures </param>
+/// <param name="data">data that needs to be transitted, the server doesn't have to parse it</param>
+/// <param name="serverTimeStamp">Not used right now, The server needs to insert it's own Timestamp into it (useful synchronization methods)</param>
 [Serializable]
-public class Data
+public class DataWrapper
 {
-	public int playerID;
-	public bool host;
-	public int type;
-	public string data;
-	//public long playerTimeStamp;
-	//public long serverTimeStamp;
+    public int objectID;
+    public bool host;
+    public DataType type;
+    public string data;
+    //public long serverTimeStamp;
 }
 
+/// <summary> DataGroup structure this is for batching multiple pieces of Data, to reduce the amount of connections
+/// </summary>
+/// <param name="datLlist">A simple list that contains multiple DataWrapper Packets</param>
 [Serializable]
-public class PositionData
+public class DataGroup
 {
-	public int id;
-	public Vector3 position;
-	public Quaternion rotation;
+    public List<DataWrapper> dataList;
 }
 
+public enum DataPrefabType {PLAYER, ENEMY, BULLETS};
+/// <summary> Example Payload, this can be replaced with something even smaller that doesn't need to be a Json string,
+/// since the Server doesn't really need to work with it
+/// </summary>
+/// <param name="position">position coordinates</param>
 [Serializable]
-public class ClientData
+public class DataPosition
 {
-	public int type;
-	public int id;
-	public Vector3 position;
-	public Quaternion rotation;	
+    public DataPrefabType prefabType;
+    public Vector3 position;
+    public Quaternion rotation;
 }
 
+public enum DataState { ACTIVE, INACTIVE, REMOVED }
+// <summary> Change the current state of the Client
+/// </summary>
+/// <param name="position">position coordinates</param>
 [Serializable]
-public class Players
+public class DataStatus
 {
-	public List<Data> players;
+    public DataState dataState;
 }
 
-[Serializable]
-public class EnemyList
-{
-	public List<Data> enemyobje;
-}
 
 public class Host : MonoBehaviour
 {
+    public GameObject playersList;
+    public GameObject enemiesList;
+    public GameObject bulletsList;
 
-	// define public game object used to visualize other players
-	public GameObject otherPlayerObject;
-	public float smoothUpdates = 10f;
-	private Vector3 prevPosition;
-	private Quaternion prevRotation;
-	private List<GameObject> otherPlayers = new List<GameObject>();
-	private int playerID = 0;
+    public string address = "localhost:8000";
 
-	public GameObject player;
-	public GameObject enemies;
-	public GameObject bullets;
+    public readonly Dictionary<int, GameObject> gameObjectList = new Dictionary<int, GameObject>();
 
-	public string address = "localhost:8000";
+    IEnumerator Start()
+    {
+        // Get all Gameobjects, so that we can order the Objects
+        GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+        playersList = GameObject.Find("Players");
+        bulletsList = GameObject.Find("Bullet");
+        enemiesList = GameObject.Find("Enemies");
 
-	IEnumerator Start()
-	{
-		// get player
-		GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
-		bullets = GameObject.Find("Bullet");
-		enemies = GameObject.Find("Enemies");
+        // connects to server
+        Debug.Log("Trying " + address);
+        if (address == "") {
+            address = "localhost:8000";
+        }
+        address = "ws://" + address.ToString();
+        WebSocket w = new WebSocket(new Uri(address));
+        yield return StartCoroutine(w.Connect());
+        Debug.Log("CONNECTED TO WEBSOCKETS");
 
-		// connect to server
-		Debug.Log("Trying " + address);
-		if (address == "") {
-			address = "localhost:8000";
-		}
-		address = "ws://" + address.ToString();
-		WebSocket w = new WebSocket(new Uri(address));
-		yield return StartCoroutine(w.Connect());
-		Debug.Log("CONNECTED TO WEBSOCKETS");
+        // wait for messages
+        // When ever a message get's received that updates the position (which are inside of the gameObject list, they will be updated)
+        while (true) {
+            // read message
+            string message = w.RecvString();
+            // check if message is not empty			
+            if (message != null) {
+                //Debug.Log("RECEIVED FROM WEBSOCKETS: " + message);
 
-		// generate random ID to have idea for each client (feels unsecure)
-		System.Guid myGUID = System.Guid.NewGuid();
-		playerID = myGUID.GetHashCode();
+                DataGroup data = JsonUtility.FromJson<DataGroup>(message);
+                foreach (DataWrapper package in data.dataList) {
+                    GameObject gameObject = gameObjectList[package.objectID];
+                    if (gameObject != null) {
+                        switch (package.type) {
+                            case DataType.DataPosition:
+                                DataPosition dataPosition = JsonUtility.FromJson<DataPosition>(package.data);
+                                gameObject.transform.position = dataPosition.position;
+                                gameObject.transform.rotation = dataPosition.rotation;
+                                break;
+                            case DataType.DataStatus:
+                                DataStatus dataStatus = JsonUtility.FromJson<DataStatus>(package.data);
+                                switch (dataStatus.dataState) {
+                                    case DataState.ACTIVE:
+                                        gameObject.SetActive(true);
+                                        break;
+                                    case DataState.INACTIVE:
+                                        gameObject.SetActive(false);
+                                        break;
+                                    case DataState.REMOVED:
+                                        Destroy(gameObject);
+                                        gameObjectList[package.objectID] = null;
+                                        break;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
 
-		// wait for messages
+            // if connection error, break the loop
+            if (w.error != null) {
+                Debug.LogError("Error: " + w.error);
+                break;
+            }
 
-		while (true)
-		{
-			// read message
-			string message = w.RecvString();
-			// check if message is not empty			
-			if (message != null)
-			{
-				//Debug.Log("RECEIVED FROM WEBSOCKETS: " + message);
+            int id = 0;
+            // check if player moved
+            if (prevPosition != player.transform.position || prevRotation != player.transform.rotation) {
+                // send update if position had changed
+                var data = new Data();
+                data.host = true;
+                data.playerID = playerID;
+                data.type = (int)DataType.Player;
 
-				/*
-				// deserialize recieved data
-				Players data = JsonUtility.FromJson<Players>(message);
-				*/
-
-				Data data = JsonUtility.FromJson<Data>(message);
-				switch (data.type)
-				{
-					case (int)DataType.Bullet:
-						//TODO: implement Other Player Bullets
-						break;
-					case (int)DataType.Player:
-						//TODO: implement Other Player Position
-						break;
-					default:
-						break;
-				}
-			}
-
-			// if connection error, break the loop
-			if (w.error != null)
-			{
-				Debug.LogError("Error: " + w.error);
-				break;
-			}
-
-			int id = 0;
-			// check if player moved
-			if (prevPosition != player.transform.position || prevRotation != player.transform.rotation)
-			{
-				// send update if position had changed
-				var data = new Data();
-				data.host = true;
-				data.playerID = playerID;
-				data.type = (int)DataType.Player;
-
-				var positionData = new PositionData();
-				positionData.id = id++;
-				positionData.position = player.transform.position;
-				positionData.rotation = player.transform.rotation;
-				data.data = JsonUtility.ToJson(positionData).ToString();
-
+                var positionData = new PositionData();
+                positionData.id = id++;
+                positionData.position = player.transform.position;
+                positionData.rotation = player.transform.rotation;
+                data.data = JsonUtility.ToJson(positionData).ToString();
 
 
-				w.SendString(JsonUtility.ToJson(data).ToString());
 
-				prevPosition = player.transform.position;
-				prevRotation = player.transform.rotation;
+                w.SendString(JsonUtility.ToJson(data).ToString());
 
-				id += 1000;
-				if (bullets != null)
-					foreach (Transform bullet in bullets.transform)
-						w.SendString(GetGameObjectJsonString(bullet.gameObject, DataType.Bullet, id++));
-				else
-					bullets = GameObject.Find("Bullet");
+                prevPosition = player.transform.position;
+                prevRotation = player.transform.rotation;
 
-				id += 1000;
-				if (enemies != null)
-					foreach (Transform enemy in enemies.transform) {
-						var e = GetGameObjectJsonString(enemy.gameObject, DataType.Enemy, id++);						
-						w.SendString(e);
-					}
+                id += 1000;
+                if (bullets != null)
+                    foreach (Transform bullet in bullets.transform)
+                        w.SendString(GetGameObjectJsonString(bullet.gameObject, DataType.Bullet, id++));
+                else
+                    bullets = GameObject.Find("Bullet");
 
-			}
+                id += 1000;
+                if (enemies != null)
+                    foreach (Transform enemy in enemies.transform) {
+                        var e = GetGameObjectJsonString(enemy.gameObject, DataType.Enemy, id++);
+                        w.SendString(e);
+                    }
+                w.Send
 
-			yield return 0;
-		}
+            }
 
-		// if error, close connection
-		w.Close();
-	}
+            yield return 0;
+        }
 
-	public string GetGameObjectJsonString(GameObject gameObject, DataType dataType, int ID)
-	{
-		var data = new Data();
-		data.type = (int)dataType;
-		data.host = true;
-		data.playerID = playerID;
+        // if error, close connection
+        w.Close();
+    }
 
-		var positionData = new PositionData();
-		positionData.id = ID;
-		positionData.position = gameObject.transform.position;
-		positionData.rotation = gameObject.transform.rotation;
-		data.data = JsonUtility.ToJson(positionData).ToString();
+    public string GetGameObjectJsonString(GameObject gameObject, DataType dataType, int ID)
+    {
+        var data = new Data();
+        data.type = (int)dataType;
+        data.host = true;
+        data.playerID = playerID;
 
-		return JsonUtility.ToJson(data).ToString();
-	}
+        var positionData = new PositionData();
+        positionData.id = ID;
+        positionData.position = gameObject.transform.position;
+        positionData.rotation = gameObject.transform.rotation;
+        data.data = JsonUtility.ToJson(positionData).ToString();
+
+        return JsonUtility.ToJson(data).ToString();
+    }
 }
