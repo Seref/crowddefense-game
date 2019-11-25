b2a1accf4f4da7ff32962a92b1014134cf687c66
Seref <Seref.G98@gmail.com>
2019-11-25 17:13:02 +0100

    fix restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,71 +5,77 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-	[Header("HUD Values")]
+    [Header("HUD Values")]
 
 
-	[Header("GameOver Screen")]
-	public GameObject GameOverScreen;
-	public TextMeshProUGUI Score;
+    [Header("GameOver Screen")]
+    public GameObject GameOverScreen;
+    public TextMeshProUGUI Score;
 
-	[Header("Multiplayer Values")]
-	public TextMeshProUGUI connectionStatus;
-	public GameObject multiplayerMenu;
+    [Header("Multiplayer Values")]
+    public TextMeshProUGUI connectionStatus;
+    public GameObject multiplayerMenu;
 
-	public TextMeshProUGUI address;
-	public Host hostGame;
+    public TextMeshProUGUI address;
+    public Host hostGame;
 
-	private bool gameOver = false;
+    private bool gameOver = false;
 
-	public void GameOver()
-	{
-		if (!gameOver) {
-			Time.timeScale = 0f;
-			gameOver = true;
-			GameOverScreen.SetActive(true);
-			var Stats = GetComponent<StatsManager>();
-			var Text = "Killed Enemies: " + Stats.Score + "\nSurvived Waves: " + Stats.Wave + "\nSurvived Time: " + Stats.PlayTime;
-			Score.text = Text;
-		}
-	}
+    void Start()
+    {
+        Application.runInBackground = true;
+        Application.targetFrameRate = 60;
+    }
 
-	public void Restart() {
-		SceneManager.LoadScene(0);
-	}
+    public void GameOver()
+    {
+        if (!gameOver) {
+            Time.timeScale = 0f;
+            gameOver = true;
+            GameOverScreen.SetActive(true);
+            var Stats = GetComponent<StatsManager>();
+            var Text = "Killed Enemies: " + Stats.Score + "\nSurvived Waves: " + Stats.Wave + "\nSurvived Time: " + Stats.PlayTime;
+            Score.text = Text;
+        }
+    }
 
-
-	//Game Data	
-	public void MultiplayerMenu()
-	{
-		if (multiplayerMenu.activeSelf)
-		{
-			Time.timeScale = 1f;
-			multiplayerMenu.SetActive(false);
-		}
-		else
-		{
-			Time.timeScale = 0f;
-			multiplayerMenu.SetActive(true);
-		}
-	}
-
-	public void HostGame()
-	{
-
-		if (address.text != "")
-			hostGame.address = address.text.Trim();
-		else
-			hostGame.address = "localhost:8000";
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
 
 
-		hostGame.enabled = true;
-		connectionStatus.text = "hosting";
-	}
+    //Game Data	
+    public void MultiplayerMenu()
+    {
+        if (multiplayerMenu.activeSelf) {
+            Time.timeScale = 1f;
+            multiplayerMenu.SetActive(false);
+        }
+        else {
+            Time.timeScale = 0f;
+            multiplayerMenu.SetActive(true);
+        }
+    }
 
-	public void JoinGame()
-	{
-		address.transform.parent = transform.parent;
-		DontDestroyOnLoad(address);
-		SceneManager.LoadScene(1, LoadSceneMode.Single);
-	}
+    public void HostGame()
+    {
+
+        if (address.text != "")
+            hostGame.address = address.text.Trim();
+        else
+            hostGame.address = "localhost:8000";
+
+
+        hostGame.enabled = true;
+        connectionStatus.text = "hosting";
+    }
+
+    public void JoinGame()
+    {
+        address.transform.parent = transform.parent;
+        DontDestroyOnLoad(address);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
 }
