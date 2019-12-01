95457aef4f75eccad885dedea482e2c23069008b
Seref <Seref.G98@gmail.com>
2019-12-01 17:12:06 +0100

    a help menu, in the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,76 +5,111 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-	[Header("GameOver Screen")]
-	public GameObject GameOverScreen;
-	public TextMeshProUGUI Score;
+    [Header("GameOver Screen")]
+    public GameObject GameOverScreen;
+    public TextMeshProUGUI Score;
 
-	[Header("Multiplayer Values")]
-	public TextMeshProUGUI connectionStatus;
-	public GameObject multiplayerMenu;
-
-	public TextMeshProUGUI address;
-	public Host hostGame;
-
-	private bool gameOver = false;
-
-	void Start()
-	{
-		Application.runInBackground = true;
-		Application.targetFrameRate = 60;
-	}
-
-	public void GameOver()
-	{
-		if (!gameOver)
-		{
-			Time.timeScale = 0f;
-			gameOver = true;
-			GameOverScreen.SetActive(true);
-			var Stats = GetComponent<StatsManager>();
-			var Text = "Killed Enemies\t" + Stats.Score + "\nSurvived Waves\t" + Stats.Wave + "\nSurvived Time\t" + Stats.PlayTime;
-			Score.text = Text;
-		}
-	}
-
-	public void Restart()
-	{
-		Time.timeScale = 1f;
-		SceneManager.LoadScene(1);
-	}
-
-	
-	public void MultiplayerMenu()
-	{
-		if (multiplayerMenu.activeSelf)
-		{
-			Time.timeScale = 1f;
-			multiplayerMenu.SetActive(false);
-		}
-		else
-		{
-			Time.timeScale = 0f;
-			multiplayerMenu.SetActive(true);
-		}
-	}
-
-	public void HostGame()
-	{
-
-		if (address.text != "")
-			hostGame.address = address.text.Trim();
-		else
-			hostGame.address = "localhost:8000";
+    [Header("Help Screen")]
+    public GameObject HelpMenuScreen;
 
 
-		hostGame.enabled = true;
-		connectionStatus.text = "hosting";
-	}
+    [Header("Multiplayer Values")]
+    public TextMeshProUGUI connectionStatus;
+    public GameObject multiplayerMenu;
 
-	public void JoinGame()
-	{
-		address.transform.parent = transform.parent;
-		DontDestroyOnLoad(address);
-		SceneManager.LoadScene(1, LoadSceneMode.Single);
-	}
+    public TextMeshProUGUI address;
+    public Host hostGame;
+
+
+    private enum Windows { GAME, GAMEOVER, HELPMENU };
+    private Windows currentWindow = Windows.GAME;
+    private bool gameOver = false;
+
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        Application.targetFrameRate = 60;
+    }
+
+    public void GameOver()
+    {
+        if (currentWindow.Equals(Windows.GAME)) {
+            PauseGame();
+            currentWindow = Windows.GAMEOVER;
+            GameOverScreen.SetActive(true);
+            var Stats = GetComponent<StatsManager>();
+            var Text = "Killed Enemies\t" + Stats.Score + "\nSurvived Waves\t" + Stats.Wave + "\nSurvived Time\t" + Stats.PlayTime;
+            Score.text = Text;
+        }
+    }
+
+    public void Restart()
+    {
+        currentWindow = Windows.GAME;
+        ContinueGame();
+        SceneManager.LoadScene(1);
+    }
+
+    public void MultiplayerMenu()
+    {
+        if (multiplayerMenu.activeSelf) {
+            Time.timeScale = 1f;
+            multiplayerMenu.SetActive(false);
+        }
+        else {
+            Time.timeScale = 0f;
+            multiplayerMenu.SetActive(true);
+        }
+    }
+
+    public void HostGame()
+    {
+
+        if (address.text != "")
+            hostGame.address = address.text.Trim();
+        else
+            hostGame.address = "localhost:8000";
+
+
+        hostGame.enabled = true;
+        connectionStatus.text = "hosting";
+    }
+
+    public void JoinGame()
+    {
+        address.transform.parent = transform.parent;
+        DontDestroyOnLoad(address);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void OpenHelpMenu()
+    {
+        if (currentWindow.Equals(Windows.GAME)){
+            HelpMenuScreen.SetActive(true);
+            currentWindow = Windows.HELPMENU;
+            PauseGame();
+        }
+    }
+
+    public void CloseHelpMenu()
+    {
+        if (currentWindow.Equals(Windows.HELPMENU)) {
+            HelpMenuScreen.SetActive(false);
+            currentWindow = Windows.GAME;
+            ContinueGame();
+        }
+    }
+
+    private bool GamePaused = false;
+    private void PauseGame()
+    {
+        GamePaused = true;
+        Time.timeScale = 1f;
+    }
+    private void ContinueGame()
+    {
+        GamePaused = false;
+        Time.timeScale = 1f;
+    }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
 	public void LoadGame()
 	{
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene("TowerDefence");
 	}
 
 	public void LoadTutorial()
