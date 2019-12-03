75a5e3d02186dd293d1af48c17430aa7f52f3e87
Seref <Seref.G98@gmail.com>
2019-12-03 16:17:40 +0100

    testing some stuff

---
+++ Assets/LoginStuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginStuff : MonoBehaviour
+{
+    public string clientID;
+    public TMPro.TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetClientID(string clientID) {
+        this.clientID = clientID;
+        text.text = clientID;
+    }
+}
