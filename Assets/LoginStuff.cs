84121fb7019afde5723ed2af7d8d74f49e75e410
Seref <Seref.G98@gmail.com>
2019-12-04 01:46:17 +0100

    JS experiments

--- Assets/LoginStuff.cs
+++ Assets/LoginStuff.cs
@@ -9,8 +9,8 @@ public class LoginStuff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		Application.ExternalEval("OnAppReady()");
+	}
 
     // Update is called once per frame
     void Update()
