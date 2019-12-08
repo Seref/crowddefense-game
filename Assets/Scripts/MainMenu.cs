c9b3df4de914c0556e1a08ddac383eb1d22c318d
Seref <Seref.G98@gmail.com>
2019-12-08 23:32:11 +0100

    big fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	public string clientID;
+	public TMPro.TextMeshProUGUI text;
+
+	void Start()
+	{
+		Application.ExternalEval("OnAppReady();");
+	}
+
 	public void LoadGame()
 	{
 		SceneManager.LoadScene("TowerDefence");
@@ -11,5 +19,11 @@ public class MainMenu : MonoBehaviour
 	public void LoadTutorial()
 	{
 		SceneManager.LoadScene("Tutorial");
+	}
+
+	public void SetClientID(string clientID)
+	{
+		this.clientID = clientID;
+		text.text = clientID;
 	}
 }
